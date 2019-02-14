1600541b74ebd8acdf4a5ec453ed3495e0eb52c7
higan96 <norihiko.oba@gmail.com>
2019-02-14 14:14:08 +0900

    git init

---
+++ MacaroniExam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MacaroniExam
+//
+//  Created by Norihiko Oba on 2019/02/14.
+//  Copyright © 2019 Norihiko Oba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
